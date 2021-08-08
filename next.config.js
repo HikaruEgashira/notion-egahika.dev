f3d662224d8cf61aac97059a896e3d897043ada7
HikaruEgashira <s1811320@gmail.com>
2021-08-08 21:38:47 +0900

    upgrade deps

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
-const withTM = require('next-transpile-modules')(['p-map'])
-
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
-module.exports = withTM(
-  withBundleAnalyzer({
-    images: {
-      domains: ['pbs.twimg.com']
-    }
-  })
-)
+module.exports = withBundleAnalyzer({
+  images: {
+    domains: ['pbs.twimg.com']
+  }
+})
