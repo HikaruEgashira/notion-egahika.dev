47664050787612b7dc3e76327df160b971aa07f4
HikaruEgashira <s1811320@gmail.com>
2021-08-22 23:24:08 +0900

    remove path /test

--- site.config.js
+++ site.config.js
@@ -44,7 +44,7 @@ module.exports = {
   // example:
   //
   pageUrlOverrides: {
-    '/test': '067dd719a912471ea9a3ac10710e7fdf'
+    // '/test': '067dd719a912471ea9a3ac10710e7fdf'
     //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   }
   // pageUrlOverrides: null
